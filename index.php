acf3e2dfe15241cd032169af72d1cb4adc1c6112
TEACHER <kellyfire611@gmail.com>
2019-08-13 18:38:13 +0700

    Tích hợp Bootstap 4 vào trang chủ

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quản lý Salomon</title>
+
+    <!-- Liên kết CSS bootstrap -->
+    <link href="public/vendor/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    
+
+
+    <!-- Hầu như tích hợp thư viện JS trước khi đóng thẻ BODY để tăng trải nghiệm người dùng -->
+    <!-- Liên kết thư viện JQuery -->
+    <script src="public/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Liên kết thư viện POPPERJS -->
+    <script src="public/vendor/popperjs/popper.min.js"></script>
+
+    <!-- Liên kết thư viện Bootstrap 4 -->
+    <script src="public/vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
