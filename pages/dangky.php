ac35c049350128f9b68a0bc561109254475171d0
TEACHER <kellyfire611@gmail.com>
2019-08-24 18:23:09 +0700

    Bổ sung FORM đăng ký

--- pages/dangky.php
+++ pages/dangky.php
@@ -4,14 +4,22 @@
 <form id="frmDangKy" name="frmDangKy" method="post" action="">
 Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
 Mật khẩu: <input type="password" name="kh_matkhau" id="kh_matkhau" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
-Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên: <input type="text" name="kh_ten" id="kh_ten" class="form-control" /><br />
+Giới tính: 
+<select name="kh_gioitinh" id="kh_gioitinh">
+    <option value="0">Nam</option>
+    <option value="1">Nữ</option>
+    <option value="2">Không công bố</option>
+</select>
+<br />
+Địa chỉ: <input type="text" name="kh_diachi" id="kh_diachi" class="form-control" /><br />
+Điện thoại: <input type="text" name="kh_dienthoai" id="kh_dienthoai" class="form-control" /><br />
+Email: <input type="text" name="kh_email" id="kh_email" class="form-control" /><br />
+Ngày sinh: <input type="text" name="kh_ngaysinh" id="kh_ngaysinh" class="form-control" /><br />
+Tháng sinh: <input type="text" name="kh_thangsinh" id="kh_thangsinh" class="form-control" /><br />
+Năm sinh: <input type="text" name="kh_namsinh" id="kh_namsinh" class="form-control" /><br />
+CMND: <input type="text" name="kh_cmnd" id="kh_cmnd" class="form-control" /><br />
+Mã kích hoạt: <input type="text" name="kh_makichhoat" id="kh_makichhoat" class="form-control" /><br />
 Trạng thái:
 <select name="kh_trangthai" id="kh_trangthai">
     <option value="0">Chưa kích hoạt</option>
