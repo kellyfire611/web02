9708c285d955fc922223efc46d723586a95b41ce
TEACHER <kellyfire611@gmail.com>
2019-08-24 18:19:47 +0700

    Bổ sung chức năng Đăng ký

---
+++ pages/dangky.php
@@ -0,0 +1,23 @@
+
+
+
+<form id="frmDangKy" name="frmDangKy" method="post" action="">
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Mật khẩu: <input type="password" name="kh_matkhau" id="kh_matkhau" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Tên đăng nhập: <input type="text" name="kh_tendangnhap" id="kh_tendangnhap" class="form-control" /><br />
+Trạng thái:
+<select name="kh_trangthai" id="kh_trangthai">
+    <option value="0">Chưa kích hoạt</option>
+    <option value="1">Đã kích hoạt</option>
+</select>
+<br />
+Có quyền Quản trị:
+<input type="checkbox" id="kh_quantri" name="kh_quantri" value="1" />
+</form>
